c570d545a964ed7d99fd4003973d7b53f9f91686
Sebastian Bargmann <xsba@bankingcircle.com>
2019-02-06 10:43:33 +0100

    testing

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+const port = process.env.PORT || 3000
+
+app.use(morgan('tiny'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.get('/', (req, res) => res.send(`PID:${process.pid}`));
